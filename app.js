8fb77ef200d84ff903f0c835afdf9ed367d27c22
nathan-nunez-5 <nathannunez42@gmail.com>
2019-06-17 14:06:07 -0400

    made testing get synchronous

--- app.js
+++ app.js
@@ -1,43 +1,58 @@
-var express = require('express');
-var http = require('http');
-var fs = require('fs');
-var bodyParser = require('body-parser');
-var app = express();
+var express = require('express')
+var http = require('http')
+var fs = require('fs')
+var bodyParser = require('body-parser')
+var app = express()
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-const port = 80;
+const port = 80
 //const hostname = '134.209.219.236';
 
-app.listen(port);
+app.listen(port)
 
-var mkdirp = require('mkdirp');
+var mkdirp = require('mkdirp')
 mkdirp('tmp', function(err) {
 
     // path exists unless there was an error
 
 });
 
-console.log('now listening to port ' + port);
+console.log('now listening to port ' + port)
 
-app.use('/assets', express.static('assets'));
+app.use('/assets', express.static('assets'))
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 app.get('/', function(request, response){
-	console.log('request was made: ' + request.url);
-	response.send('hi team! this is the homepage');
-});
+	console.log('request was made: ' + request.url)
+	response.send('hi team! this is the homepage')
+})
 
 app.get('/testing', function(request, response){
-	console.log('request was made: ' + request.url);
-	//response.sendFile(__dirname + '/testing.html');
-	response.render('testing', {up: request.query, data_transcript: 'this is some testing value: ', data_value: 42});
+	console.log('request was made: ' + request.url)
+	var start_code = ''
+	var start_examples = ''
+	// var items = fs.readdirSync('./tmp')
+	// console.log(items)
+	// if(items.includes('code.txt')){
+	// 	start_code = fs.readFileSync('./tmp/code.txt', 'utf8')
+	// 	console.log('code.txt has been uploaded')
+	// }
+	// if(items.includes('examples.txt')){
+	// 	start_examples = fs.readFileSync('./tmp/examples.txt', 'utf8')
+	// 	console.log('examples.txt has been uploaded')
+	// }
+	var up = {
+		code: start_code,
+		examples: start_examples
+	}
+	response.render('testing', {up: up});
+	console.log(up)
 
-});
+})
 
 var backendFxns = require('live-backend-nfn5')
-
 var reeval = backendFxns.reeval
 var updateCodeEvalJS= backendFxns.updateCodeEvalJS
 
