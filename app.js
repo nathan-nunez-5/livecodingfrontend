74f7bf831a8551a7332c2132d90505083352d2a8
nathan-nunez-5 <nathannunez42@gmail.com>
2019-07-10 15:52:26 -0400

    concurrency for 2 people works via cookies

--- app.js
+++ app.js
@@ -2,37 +2,57 @@ var express = require('express')
 var http = require('http')
 var fs = require('fs')
 var bodyParser = require('body-parser')
-
-//var cookieParser = require('cookie-parser');
-
+var cookieParser = require('cookie-parser');
 var app = express()
-//app.use(cookieParser());
-
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-//cookie grabbing
-// app.get('/cookie',function(req, res){
-//      res.cookie(cookie_name , 'cookie_value').send('Cookie is set');
-// });
-
-
 const port = 80
-
 app.listen(port);
+//console.log('now listening to port ' + port)
 
 //read up on this
 var mkdirp = require('mkdirp')
 mkdirp('tmp', function(err) {
-
     // path exists unless there was an error
-
 });
 
-console.log('now listening to port ' + port)
+//app.use('/assets', express.static('assets'))
+app.set('view engine', 'ejs')
+app.use(cookieParser());
+
+app.use(function (request, response, next) {
+  // check if client sent cookie
+  var cookie = request.cookies.uCookie;
+  if (cookie === undefined)
+  {
+    // no: set a new cookie
+    var randomNumber = Math.random().toString();
+    randomNumber = randomNumber.substring(2,randomNumber.length);
+    response.cookie('uCookie',randomNumber);
+    console.log('cookie created successfully');
+  }
+  else
+  {
+    // yes, cookie was already present
+    console.log('cookie exists', cookie);
+  }
+  next(); // <-- important!
+});
+
+// let static middleware do its job
+app.use(function (request, response, next){
+	var userFolder = 'tmp/' + request.cookies.uCookie
+  if(userFolder !== undefined){
+		mkdirp(userFolder, function(err) {
+			//path exists unless it don't
+		})
+	}
+
+
+	next();
+});
 
 app.use('/assets', express.static('assets'))
-
-app.set('view engine', 'ejs')
 
 app.get('/', function(request, response){
 	console.log('request was made: ' + request.url)
@@ -77,8 +97,10 @@ app.post('/testing', urlencodedParser, function(request, response){
 	var up_examples = request.body.up_examples;
 
 	//savefiles in hidden folder tmp
-	fs.writeFileSync('tmp/code.js', up_code)
-	fs.writeFileSync('tmp/code.js.examples', up_examples)
+	var userFolder = 'tmp/' + request.cookies.uCookie + '/'
+	console.log('this folder' + userFolder)
+	fs.writeFileSync(userFolder + 'code.js', up_code)
+	fs.writeFileSync(userFolder + 'code.js.examples', up_examples)
 	// console.log(up_code)
 	// console.log(up_examples)
 
@@ -90,7 +112,7 @@ app.post('/testing', urlencodedParser, function(request, response){
 	// 	examples: newExamples
 	// }
 
-  var path = 'tmp/code.js.sl'
+  var path = userFolder +'code.js.sl'
 	//console.log("begin: updateCodeEvalJS")
   var res = updateCodeEvalJS(up_code, parseExamples(up_examples), path)
 
