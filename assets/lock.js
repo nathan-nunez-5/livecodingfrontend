7fc1fcc225f2b1a63eb0133bbd956f7f066e40e5
nathan-nunez-5 <nathannunez42@gmail.com>
2019-07-24 16:56:51 -0400

    added codemirror to testing.ejs

--- assets/lock.js
+++ assets/lock.js
@@ -5,9 +5,6 @@ function sendPost(){
   lock = true
   var code = document.getElementById('up_code').value
   var examples = document.getElementById('up_examples').value
-  // console.log('hello')
-  // console.log(code)
-  // console.log(examples)
   if(code == '' || examples == ''){ console.log('return'); lock = false; return }
   var params = {
     up_code: code,
